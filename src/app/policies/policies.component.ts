737b34f6671889fbe859de33dea5ed94d7961a93
YogeshKumarV <125454507+YogeshKumarV@users.noreply.github.com>
2024-10-28 19:17:39 +0530

    policies component commit

--- src/app/policies/policies.component.ts
+++ src/app/policies/policies.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-policies',
@@ -6,6 +7,72 @@ import { Component } from '@angular/core';
   styleUrl: './policies.component.css'
 })
 export class PoliciesComponent {
+
+
+
+  constructor(private formBuilder: FormBuilder) {
+    this.formGroupPolicies = this.formBuilder.group({
+      requestPolicies: [null],
+      requestPolicies1: [null],
+      responsePolicies: [null]
+    })
+  }
+
+
+  formGroupPolicies: FormGroup;
+
+  @Output() formSubmitted = new EventEmitter<any>();
+  submitForm() {
+    if (this.formGroupPolicies.valid) {
+      this.formSubmitted.emit(this.formGroupPolicies.value);
+    }
+  }
+
+  parameterArray1: any = [];
+
+  addParameterReqP1() {
+    const queryParamsValue = this.formGroupPolicies.get('requestPolicies1')?.value;
+
+    if (queryParamsValue) {
+      this.parameterArray1.push(queryParamsValue);
+      this.formGroupPolicies.get('requestPolicies1')?.reset();
+    }
+  }
+  removeParameterReqP1(index: number) {
+    this.parameterArray1.splice(index, 1);
+  }
+
+  parameterArray2: any = [];
+
+  addParameterResP() {
+    const queryParamsValue = this.formGroupPolicies.get('responsePolicies')?.value;
+
+    if (queryParamsValue) {
+      this.parameterArray2.push(queryParamsValue);
+      this.formGroupPolicies.get('responsePolicies')?.reset();
+    }
+  }
+  removeParameterResP(index: number) {
+    this.parameterArray2.splice(index, 1);
+  }
+
+
+
+  parameterArray3: any = [];
+
+  addParameterReqP() {
+    const queryParamsValue = this.formGroupPolicies.get('requestPolicies')?.value;
+
+    if (queryParamsValue) {
+      this.parameterArray3.push(queryParamsValue);
+      this.formGroupPolicies.get('requestPolicies')?.reset();
+    }
+  }
+  removeParameterReqP(index: number) {
+    this.parameterArray3.splice(index, 1);
+  }
+
+
   isSpFilterEnabled = false; // Initially false
 
   onToggleChange(event: any) {

a076f4e0ba04ff0e52c48784fd9c0d630e37e847
balajipedapudi <balajipedapudi@gmail.com>
2024-11-18 16:00:11 +0530

    api-key

--- src/app/api-keys/api-keys.component.ts
+++ src/app/api-keys/api-keys.component.ts
@@ -1,20 +1,53 @@
-import { Component, Inject } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { AfterViewInit, Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-api-keys',
   templateUrl: './api-keys.component.html',
-  styleUrl: './api-keys.component.css'
+  styleUrl:'./api-keys.component.css'
 })
-export class ApiKeysComponent {
+export class ApiKeysComponent{
 
-  apikeyFormGroup:FormGroup;
 
-  constructor(private fb:FormBuilder){
+  apiKeysForm: FormGroup;
+  keysArray:any[] = [];
+  rolesArray:any[]=[];
 
-    this.apikeyFormGroup = this.fb.group({
-      isAPIKeyAuthActive:[false]
+  constructor(private fb: FormBuilder) {
+    this.apiKeysForm = this.fb.group({
+      isAPIKeyAuthActive:[false],
+      APIKey:[],
+      role:[],
+      rolesArrayValue:[[]],
+      description:[]
     })
   }
 
-}
+  
+  addParameter(fieldName: 'role'){
+    const fieldValue = this.apiKeysForm.get(fieldName)?.value;
+    
+    if(fieldName){
+      if(fieldName ==='role'){
+        this.rolesArray.push(fieldValue);
+        this.apiKeysForm.get('rolesArrayValue')?.setValue([...this.rolesArray])
+      }
+    }
+
+    this.apiKeysForm.get(fieldName)?.reset();
+  }
+
+  removeParameter(index:any, fieldName:'role'){
+    this.rolesArray.splice(index,1);
+    this.apiKeysForm.get('rolesArrayValue')?.setValue([...this.rolesArray]);
+  } 
+
+
+  addKey(){
+    this.keysArray.push(this.apiKeysForm.value)
+    console.log(this.keysArray);
+    
+  }
+
+  }
+  
